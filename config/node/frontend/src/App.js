1b8b8907515fb248be5b0c010a803f41d58bbc77
Adiaxi <u56129@student.wat.edu.pl>
2025-12-08 20:41:59 +0100

    feat: input of home component and style for it

--- config/node/frontend/src/App.js
+++ config/node/frontend/src/App.js
@@ -1,10 +1,18 @@
+import {Suspense} from "react";
 import Home from "./pages/Home";
 import './styles/style.scss';
+import {CircularProgress} from "@mui/material"
+
+import {RouterProvider} from 'react-router-dom'
+import routes from './routes/Router'
 
 function App() {
   return (
     <div className="app">
-        <Home/>
+        <Suspense fallback={<CircularProgress color="secondary" />}>
+
+            <RouterProvider router={routes}/>
+        </Suspense>
 
     </div>
   );

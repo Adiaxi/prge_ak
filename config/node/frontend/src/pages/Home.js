dbe49aff06f1e487703b29fdff4e2aa1528caff8
Adiaxi <u56129@student.wat.edu.pl>
2025-12-08 21:01:17 +0100

    feat: button link to service page

--- config/node/frontend/src/pages/Home.js
+++ config/node/frontend/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Typography, Button} from "@mui/material"
+import {Link} from 'react-router-dom';
 
 function Home(props) {
     return (
@@ -9,7 +10,11 @@ function Home(props) {
                 Geoportal tematyczny poświęcony dany przestrzennym
             </Typography>
 
-            <Button className='home__button' variant='contained' size='large' >
+            <Button
+                className='home__button'
+                variant='contained'
+                size='large'
+                component={Link} to='services'>
                 START
             </Button>
 

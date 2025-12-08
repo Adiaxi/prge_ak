ad826f4e0a8731a7367b2371cf8d712c08f8ae6d
Adiaxi <u56129@student.wat.edu.pl>
2025-12-08 19:59:11 +0100

    feat: input of home component and style for it

--- config/node/frontend/src/pages/Home.js
+++ config/node/frontend/src/pages/Home.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import {Typography, Button} from "@mui/material"
 
 function Home(props) {
     return (
-        <div>strona startowa react</div>
+        <div className='home'>
+            <h1 className='home__header'>GEOPORTAL</h1>
+            <Typography>
+                Geoportal tematyczny poświęcony dany przestrzennym
+            </Typography>
+
+            <Button className='home__button' variant='contained' size='large' >
+                START
+            </Button>
+
+        </div>
     );
 }
 
